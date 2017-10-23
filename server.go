34866fbd56630a96b05a58442d5466a9afcfa86b
David Christopher <roronoalance@gmail.com>
2017-10-23 12:25:38 +0700

    add endpoint change server protocol from udp to tcp change input format standard to protobuf

--- server.go
+++ server.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"bytes"
 	"net"
 	"time"
 
 	"github.com/pkg/errors"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/golang/protobuf/proto"
+	"github.com/bukalapak/prometheus-aggregator/protomodel"
 )
 
-type sampleHandler func(samples *sample) error
+type sampleHandler func(samples *protomodel.Sample) error
+//type sampleHandler func(samples int) error
 
 type server struct {
 	sampleHandler sampleHandler
@@ -21,10 +23,10 @@ type server struct {
 	metricRequestHandlingDuration prometheus.Summary
 }
 
-// newServer is factory for UDP server for incoming metrics data
+// newServer is factory for TCP server for incoming metrics data
 //
 // handler is a function of sampleHandler type responsible for dealing with incoming samples
-// bs is a UDP buffer size in bytes
+// bs is a TCP buffer size in bytes
 func newServer(handler sampleHandler, bs int) *server {
 	s := server{
 		sampleHandler: handler,
@@ -54,42 +56,43 @@ func newServer(handler sampleHandler, bs int) *server {
 	return &s
 }
 
-func (s *server) Listen(ip string, port int) error {
-	listenAddr := net.UDPAddr{
-		Port: port,
-		IP:   net.ParseIP(ip),
-	}
-	conn, err := net.ListenUDP("udp", &listenAddr)
+func (s *server) Listen(ip string, port string) error {
+	laddr, err := net.ResolveTCPAddr("tcp", ip+":"+port)
 	if err != nil {
 		return errors.Wrap(err, "opening server socket failed")
 	}
-
-	go func() {
-		var (
-			reader *bytes.Reader
-			tS     time.Time
-		)
-
-		for {
-			n, _, _ := conn.ReadFromUDP(s.buf)
-
-			tS = time.Now()
-
-			s.metricRequestsTotal.Inc()
-
-			reader = bytes.NewReader(s.buf[:n])
-
-			samples, _ := parseSample(reader)
-
-			s.metricSamplesTotal.Add(float64(len(samples)))
-
-			for _, sample := range samples {
-				_ = s.sampleHandler(sample)
-			}
-
-			s.metricRequestHandlingDuration.Observe(float64(time.Since(tS).Nanoseconds()))
-		}
-	}()
-
+	conn, err := net.ListenTCP("tcp",laddr)
+	if err != nil {
+		return errors.Wrap(err, "opening server socket failed")
+	}
+	go s.handleRequest(conn)
+	
 	return nil
 }
+
+func (s *server) handleRequest(tcp *net.TCPListener) {		
+	var (	
+		tS     time.Time
+	)
+	conn, err := tcp.Accept()
+	if err != nil {
+
+	}
+	for{
+		reqLen, err := conn.Read(s.buf)
+	 	if err != nil {
+			return
+		}
+		tS = time.Now()
+		s.metricRequestsTotal.Inc()
+		protodata := &protomodel.Sample{}
+		err = proto.Unmarshal(s.buf[0:reqLen], protodata)
+		s.metricSamplesTotal.Add(float64(1))
+		err = s.sampleHandler(protodata)
+		if err != nil{
+
+		}
+		s.metricRequestHandlingDuration.Observe(float64(time.Since(tS).Nanoseconds()))
+	}
+}
+
