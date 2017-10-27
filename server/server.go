a3141b9fef75ad7660c04a8291289b258f72502d
davidchrisx <roronoalance@gmail.com>
2017-10-27 17:26:19 +0700

    add metrics for protor

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"net"
 	"syscall"
+	"time"
 
 	pr "github.com/bukalapak/prometheus-aggregator"
 	"github.com/bukalapak/prometheus-aggregator/protomodel"
@@ -21,6 +22,22 @@ func New(pProtor *pr.Protor) *Server {
 		buf:    make([]byte, 1024),
 	}
 	return &s
+}
+
+func MetricProtor(Name string, Kind string, Value float64, start time.Time) *pr.Sample {
+	HD := []string{}
+	if Kind == "h" {
+		Value = float64(time.Since(start).Nanoseconds())
+		HD = []string{".005", ".01", ".025", ".05", ".1", ".25", ".5", "1", "2.5", "5", "10"}
+	}
+	return &pr.Sample{
+		Service:      "metrics",
+		Name:         Name,
+		Kind:         Kind,
+		Label:        make(map[string]string),
+		Value:        Value,
+		HistogramDef: HD,
+	}
 }
 
 func (s *Server) Run(ip string, port string) {
@@ -42,6 +59,7 @@ func (s *Server) Run(ip string, port string) {
 }
 
 func (s *Server) handleRequest(conn net.Conn) {
+	tS := time.Now()
 	reqLen, err := conn.Read(s.buf)
 	if err != nil {
 		exitOnFatal(err, "fail at reading")
@@ -56,6 +74,8 @@ func (s *Server) handleRequest(conn net.Conn) {
 	if err != nil {
 		exitOnFatal(err, "fail while writing registry")
 	}
+	err = s.Protor.WriteToRegistry(MetricProtor("app_request_handle_duration_ns", "h", 1, tS))
+	err = s.Protor.WriteToRegistry(MetricProtor("app_total_sample", "c", 1, tS))
 }
 
 func ProtoToSample(pd *protomodel.Sample) *pr.Sample {
